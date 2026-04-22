3365cd90488b9602e5597b679d325e6b439f304d
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-04-22 17:58:17 +0530

    feat: integrate OpenAI model for summarizing loaded text documents

--- CampusX/text_loader.py
+++ CampusX/text_loader.py
@@ -1,4 +1,19 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+prompt = PromptTemplate(
+    template="Write a summary for the following paragraph - {poem}",
+    input_variables=['paragraph']
+)
+
+parser = StrOutputParser()
 
 loader = TextLoader("../data/text_files/text1.txt",encoding="utf-8")
 
@@ -9,3 +24,8 @@ print(len(docs))
 
 print(docs[0])
 print(docs[0].page_content)
+
+
+chain = prompt | model | parser
+
+print(chain.invoke({'poem':docs[0].page_content}))
