6f91398269da1de38fa8f48e02319bf8314bc7b5
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-04-22 20:25:19 +0530

    feat: print type, length, and content of loaded CSV data

--- CampusX/csv_loader.py
+++ CampusX/csv_loader.py
@@ -2,3 +2,10 @@ from langchain_community.document_loaders import CSVLoader
 
 loader = CSVLoader("../data/csv/file.csv")
 
+data = loader.load()
+
+# print(data)
+print(type(data))
+print(len(data))
+print(data[0].page_content)
+print(data[0].metadata)
