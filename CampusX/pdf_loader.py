ba20f1925d198b06c54bbfb187f26790d33a3563
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-04-22 18:39:32 +0530

    feat: display page content and metadata for the first loaded PDF document

--- CampusX/pdf_loader.py
+++ CampusX/pdf_loader.py
@@ -4,6 +4,9 @@ doc = PyMuPDFLoader("../data/pdf/Lecture 3 Operators in JAVA.pdf")
 
 pdf = doc.load()
 
-print(pdf)
-print(type(pdf))
-print(len(pdf))
+# print(pdf)
+# print(type(pdf))
+# print(len(pdf))
+
+print(doc[0].page_content)
+print(doc[0].metadata)
