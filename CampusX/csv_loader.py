a748006aa19fe8e5db787e062631e4d67fe7fdb0
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-04-22 20:27:40 +0530

    feat: switch to lazy loading for CSV data and print page content

--- CampusX/csv_loader.py
+++ CampusX/csv_loader.py
@@ -2,10 +2,12 @@ from langchain_community.document_loaders import CSVLoader
 
 loader = CSVLoader("../data/csv/file.csv")
 
-data = loader.load()
+data = loader.lazy_load()
 
 # print(data)
-print(type(data))
-print(len(data))
-print(data[0].page_content)
-print(data[0].metadata)
+# print(type(data))
+# print(len(data))
+# print(data[0].page_content)
+# print(data[0].metadata)
+for item in data:
+    print(item.page_content)
