c918114a31b2d5d9241848d4d0736134f38c8d86
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-04-22 18:08:13 +0530

    feat: switch to Google Generative AI model for text summarization

--- CampusX/text_loader.py
+++ CampusX/text_loader.py
@@ -1,12 +1,12 @@
 from langchain_community.document_loaders import TextLoader
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI()
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
 prompt = PromptTemplate(
     template="Write a summary for the following paragraph - {paragraph}",
