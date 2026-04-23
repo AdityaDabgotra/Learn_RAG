a7b17c7fe87e1df536d9054592c67c2612e88e9c
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-04-23 12:13:06 +0530

    feat: add document structure based text splitting functionality

---
+++ CampusX/document_structure_based_text_splitting.py
@@ -0,0 +1,20 @@
+from langchain_text_splitters import MarkdownTextSplitter
+
+splitter = MarkdownTextSplitter(
+    chunk_size = 50,
+    chunk_overlap = 0,
+)
+
+text = """
+### learning RAG
+
+functions of rag:
+- `rag_retrieve()`: Retrieve relevant documents from a knowledge base based on a query.
+- `rag_generate()`: Generate a response based on the retrieved documents and the query.
+- `rag()`: A high-level function that combines retrieval and generation in one step.
+
+### Example usage
+"""
+data = splitter.split_text(text)
+
+print(data)
---
+++ readme.md
@@ -0,0 +1,8 @@
+### learning RAG
+
+functions of rag:
+- `rag_retrieve()`: Retrieve relevant documents from a knowledge base based on a query.
+- `rag_generate()`: Generate a response based on the retrieved documents and the query.
+- `rag()`: A high-level function that combines retrieval and generation in one step.
+
+### Example usage
