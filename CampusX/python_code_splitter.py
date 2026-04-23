776ed4ad6c7d3c5f788f7e49b280a01e856ebd6d
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-04-23 13:00:27 +0530

    feat: implement Python code splitter for processing and chunking code snippets

---
+++ CampusX/python_code_splitter.py
@@ -0,0 +1,34 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter,Language
+
+text = """
+class Student:
+    def __init__(self,name,age,grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+    
+    def get_details(self):
+        return self.name
+    
+    def isPassing(self):
+        return self.grade >= 6.0
+
+student1 = Student('Aditya',22,8.8)
+print(student1.get_details)
+
+if student1.isPassing():
+    print("Student will pass)
+else print("Student will fail)
+"""
+
+splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON,
+    chunk_size = 300,
+    chunk_overlap = 0,
+)
+
+chunks = splitter.split_text(text)
+
+print(chunks)
+print(len(chunks))
+print(chunks[0])
