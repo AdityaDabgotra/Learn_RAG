d0163d56233d4fe77bf8c72a34bdf5d4ff2a9091
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-04-26 12:14:55 +0530

    feat: implement document processing and retrieval with vector store integration

--- practice.py
+++ practice.py
@@ -1,19 +1,53 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
 
-loader = PyMuPDFLoader("gunaho ka devta.pdf")
+load_dotenv()
 
-data = loader.lazy_load()
+def process():
+    loader = PyMuPDFLoader("gunaho ka devta.pdf")
 
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=350,
-    chunk_overlap=20,
-)
+    data = loader.lazy_load()
 
-splitter_text = []
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=350,
+        chunk_overlap=20,
+    )
 
-for doc in data:
-    chunks = splitter.split_documents([doc])
-    splitter_text.extend(chunks)
+    splitter_text = []
 
-print(splitter_text)
+    for doc in data:
+        chunks = splitter.split_documents([doc])
+        splitter_text.extend(chunks)
+
+
+    vector_store = Chroma(
+        embedding_function=HuggingFaceEmbeddings(model_name="l3cube-pune/indic-sentence-bert-nli"),
+        persist_directory="Chroma DB",
+        collection_name="Gunaho-ka-devta"
+    )
+
+    vector_store.add_documents(splitter_text)
+
+    return vector_store
+
+
+def retrieval(question,vector_store):
+    response = vector_store.similarity_search(
+        query = question,
+        k = 5
+    )
+    return response
+
+if __name__ == "__main__":
+    store = process()
+    print("Document Scanned Successfully !!\n\n")
+    while(True):
+        ques = input("Enter your Question:\t")
+        if ques.lower().strip() == 'exit':
+            break
+        response = retrieval(ques,store)
+        print(response)
+
