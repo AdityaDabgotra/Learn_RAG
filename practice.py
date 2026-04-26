2532b150efc378c4d98869f17c551ac60b3fb72f
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-04-26 11:48:39 +0530

    feat: implement text splitting for PDF document pages with specified chunk size and overlap

--- practice.py
+++ practice.py
@@ -1,8 +1,18 @@
 from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 loader = PyMuPDFLoader("gunaho ka devta.pdf")
 
 data = loader.lazy_load()
 
+splitter_text = []
 for i in data:
-    print(i)
+    page = RecursiveCharacterTextSplitter(
+        chunk_size = 350,
+        chunk_overlap = 20,
+    )
+    
+    splitter_text.append(page.split_documents(i))
+
+print(splitter_text)
+
